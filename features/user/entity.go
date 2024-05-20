27959e800e95999399ba5cc25220daaae3745495
bagasalfaristo <bagas.a.putra31@gmail.com>
2024-05-20 21:31:21 +0700

    revisi penamaan pada models dan entity

--- features/user/entity.go
+++ features/user/entity.go
@@ -5,14 +5,14 @@ import (
 )
 
 type Core struct {
-	ID             uint
-	Name           string
-	Email          string
-	Password       string
-	PhoneNumber    string
-	ProfilePicture string
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
+	ID           uint
+	Name         string
+	Email        string
+	Password     string
+	NomorTelepon string
+	Foto         string
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
 }
 
 type DataInterface interface {
